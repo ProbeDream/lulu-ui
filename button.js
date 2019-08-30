4de10705006cd6ad7fc05c302011431f57cb886f
CharlieSmith97 <Gump1016@163.com>
2019-08-30 18:32:40 +0800

    将对应的l-button 改为了单文件组件的形式

--- button.js
+++
@@ -1,3 +0,0 @@
-Vue.component('l-button',{
-    template:`<button class="l-button">hi</button>`
-})
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Button from './button';
+Vue.component('l-button',Button);
+let app = new Vue({
+    el: '#app',
+})
