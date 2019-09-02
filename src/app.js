0b0ddbc69ac0a57caefb5e8397830b15f2204f4f
CharlieSmith97 <Gump1016@163.com>
2019-09-02 21:23:30 +0800

    为Input组件添加测试用例

--- src/app.js
+++ src/app.js
@@ -7,4 +7,19 @@ Vue.component('l-button',Button);
 Vue.component('l-icon',Icon);
 Vue.component('l-button-group',ButtonGroup);
 Vue.component('l-input',Input);
-let app = new Vue({el: '#app', data:{loading01:false,loading02:true,loading03:false}});
+let app = new Vue({el: '#app',
+    data:{loading01:false,loading02:true,loading03:false},
+    created() {
+        setTimeout(()=>{
+            let event = new Event('change');
+            let inputElement = this.$el.querySelector('input');
+            inputElement.dispatchEvent(event);
+            console.log('dispatchEvent Event Done!');
+        },1000);
+    },
+    methods:{
+        inputChange(event){
+            console.log(event);
+        }
+    }
+});
