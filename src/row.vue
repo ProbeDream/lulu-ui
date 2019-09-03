985553292221ecb29355ff6d06b11425fd5eda45
CharlieSmith97 <Gump1016@163.com>
2019-09-03 14:28:03 +0800

    添加了关于row组件的样式align=left/right/center属性!

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="rowStyle">
+    <div class="row" :style="rowStyle" :class="rowClass">
         <slot></slot>
     </div>
 </template>
@@ -7,10 +7,22 @@
 <script>
 export default {
     name:"lRow",
+    props:{
+      gutter:{
+        type:[Number,String]
+      },align:{
+          type:String,validator(value) {
+                return ['left','right','center'].indexOf(value) !== -1;
+            }
+        }
+    },
     computed:{
         rowStyle(){
             let {gutter} = this;
             return {marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'};
+        },rowClass(){
+           let {align} = this;
+           return [align && `align-${align}`]
         }
     },
     props:{gutter:{type:[String,Number]}},
@@ -22,5 +34,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.row{display: flex;}
+.row{
+    display: flex;
+    &.align-left{
+        justify-content: flex-start;
+    }
+    &.align-right{
+        justify-content: flex-end;
+    }
+    &.align-center{
+        justify-content: center;
+}
+}
 </style>
