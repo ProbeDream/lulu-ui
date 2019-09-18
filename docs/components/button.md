22b269cf0f3781ab79ad57e0311e5f6f24ee3002
ProbeDream <Gump1016@163.com>
2019-09-18 13:10:31 +0800

    LuLuUI的管网基本界面!

---
+++ docs/components/button.md
@@ -0,0 +1,7 @@
+---
+title:button 
+sidebarDepth:2
+---
+# 按钮
+使用方式:
+<l-button></l-button>    
--- src/button.vue
+++ src/button.vue
@@ -2,7 +2,7 @@
         <button class="l-button"  :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
             <l-icon class="icon" v-if="icon && !loading" :name="icon"></l-icon>
             <l-icon name="Loading" v-if="loading" class="loading icon"></l-icon>
-            <div class="content">
+            <div class="l-button-content">
                 <slot></slot>
             </div>
         </button>
@@ -28,23 +28,31 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+    $font-size: 14px;
+    $button-height: 32px;
+    $button-bg: white;
+    $button-active-bg: #eee;
+    $border-radius: 4px;
+    $color: #333;
+    $border-color: #999;
+    $border-color-hover: #666;
     @keyframes  spin {
         0%{transform:rotate(0deg);}
         100%{transform:rotate(360deg);}
     }
     .l-button {
-        font-size: var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);background: var(--button-bg);display: inline-flex;
+        font-size: $font-size;height: $button-height;padding: 0 1em;border-radius: $border-radius;
+        border: 1px solid $border-color;background: $button-bg;display: inline-flex;
         justify-content: center;align-items: center;vertical-align: middle;
-        &:hover {border-color: var(--border-color-hover);}
-        &:active {background-color: var(--button-active-bg);}
+        &:hover {border-color: $border-color-hover;}
+        &:active {background-color: $button-active-bg;}
         &:focus {outline: none;}
         .loading{animation:spin 1s infinite linear;}
         > .icon{order: 1;}
-        > .content{order: 2;margin-right: .1em;}
+        > .l-button-content{order: 2;margin-right: .1em;}
         &.icon-right{
             > .icon{order: 2;margin-left: .1em;}
-            > .content{order: 1;}
+            > .l-button-content{order: 1;}
         }
     }
 </style>
