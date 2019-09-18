0f8f92bb5f42d61a396667efe409e36b20285e48
ProbeDream <Gump1016@163.com>
2019-09-18 09:00:38 +0800

    为collapse组件添加对应的Selected选项!

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -11,27 +11,30 @@
 export default {
     name:'luluCollapseItem',
     props:{
-        title:{type:String,required:true}
+        title:{type:String,required:true},
+        name:{type:String,required:true}
     },data(){
         return {open:false};
     },inject: ['eventBus'],
     mounted(){
-        this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
-               if (vm !== this){
+        this.eventBus && this.eventBus.$on('update:selected',(name)=>{
+               if (name !== this.name){
                    this.close();
+               }else{
+                   this.show()
                }
         });
     },methods:{
         toggle(){
             if (this.open){
                 this.open = false;
-                this.eventBus && this.eventBus.$emit('update:selected',this);
             }else{
-                this.open = true;
-                this.eventBus && this.eventBus.$emit('update:selected',this);
+                this.eventBus && this.eventBus.$emit('update:selected',this.name);
             }
         },close(){
             this.open = false;
+        },show(){
+            this.open = true;
         }
     }
 }
--- src/collapse.vue
+++ src/collapse.vue
@@ -9,11 +9,17 @@ import Vue from 'vue';
 export default {
     name:'luluCollapse',
     props:{
-        single:{type:Boolean,default:false}
+        single:{type:Boolean,default:false},
+        selected:{type:String}
     },data(){
         return {eventBus:new Vue()};
     },provide(){
-        if (this.single){return {eventBus:this.eventBus}}
+        return {eventBus:this.eventBus}
+    },mounted() {
+        this.eventBus.$emit('update:selected',this.selected);
+        this.eventBus.$on('update:selected',name=>{
+            this.$emit('update:Selected',name);
+        })
     }
 }
 </script>
